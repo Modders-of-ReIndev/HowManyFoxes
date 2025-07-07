07728d0a95c2506f560fe384b3a585dc813ecba3
Tracy [Stacktrace] <173243368+tracystacktrace@users.noreply.github.com>
2025-07-07 19:25:56 +0500

    correct (i think) chance calculation

--- src/main/java/com/fox2code/howmanyfoxes/hmi/tabs/TabLootHints.java
+++ src/main/java/com/fox2code/howmanyfoxes/hmi/tabs/TabLootHints.java
@@ -13,7 +13,8 @@ import net.minecraft.common.util.i18n.StringTranslate;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Random;
 
 public class TabLootHints extends TabWithTexture {
 
@@ -23,11 +24,18 @@ public class TabLootHints extends TabWithTexture {
         public final ItemStack resourceItem;
         public final LootTable lootTable;
         public final String customName;
+        public final int totalWeight;
 
         public FancyPackedLoot(ItemStack resourceItem, String customName, LootTable lootTable) {
             this.resourceItem = resourceItem;
             this.lootTable = lootTable;
             this.customName = customName;
+
+            int weight_accumulation = 0;
+            for (int i = 0; i < lootTable.getLootTableEntries().size(); i++) {
+                weight_accumulation += lootTable.getLootTableEntries().get(i).getItemWeight();
+            }
+            this.totalWeight = weight_accumulation;
         }
 
         public FancyPackedLoot(ItemStack resourceItem, LootTable lootTable) {
@@ -37,7 +45,7 @@ public class TabLootHints extends TabWithTexture {
         public ItemStack[] getLootAsArray() {
             ItemStack[] stacks = new ItemStack[lootTable.getLootTableEntries().size()];
 
-            for(int i = 0; i < stacks.length; i++) {
+            for (int i = 0; i < stacks.length; i++) {
                 stacks[i] = fakeGenerateLoot(lootTable.getLootTableEntries().get(i));
             }
 
@@ -47,14 +55,14 @@ public class TabLootHints extends TabWithTexture {
         public ItemStack[][] getLootAsSplitArrays(int chunkSize) {
             final ItemStack[] source = this.getLootAsArray();
 
-            if(source.length < chunkSize) {
-                return new ItemStack[][] {source};
+            if (source.length < chunkSize) {
+                return new ItemStack[][]{source};
             }
 
             final int numberOfChunks = (int) Math.ceil((double) source.length / chunkSize);
             final ItemStack[][] output = new ItemStack[numberOfChunks][];
 
-            for(int i = 0; i < numberOfChunks; i++) {
+            for (int i = 0; i < numberOfChunks; i++) {
                 int start = i * chunkSize;
                 int end = Math.min(source.length, start + chunkSize);
                 output[i] = Arrays.copyOfRange(source, start, end);
@@ -69,12 +77,12 @@ public class TabLootHints extends TabWithTexture {
         }
 
         public boolean hasSuchLoot(ItemStack itemStack) {
-            if(itemStack == null) {
+            if (itemStack == null) {
                 return false;
             }
 
-            for(LootTableEntry table : this.lootTable.getLootTableEntries()) {
-                if(table.getItemStack().getItemID() == itemStack.getItemID()) {
+            for (LootTableEntry table : this.lootTable.getLootTableEntries()) {
+                if (table.getItemStack().getItemID() == itemStack.getItemID()) {
                     return true;
                 }
             }
@@ -84,7 +92,7 @@ public class TabLootHints extends TabWithTexture {
 
         public ItemStack getDisplayItemStack() {
             ItemStack stack = this.resourceItem.copy();
-            if(this.customName != null) {
+            if (this.customName != null) {
                 stack.setItemName(StringTranslate.getInstance().translateKey(this.customName));
             }
             return stack;
@@ -93,9 +101,9 @@ public class TabLootHints extends TabWithTexture {
         private ItemStack fakeGenerateLoot(LootTableEntry entry) {
             final ItemStack stack = entry.getItemStack().copy();
 
-            if(RANDOM.nextDouble() * 100.0 < entry.getDropChance() * 100.0) {
+            if (RANDOM.nextDouble() * 100.0 < entry.getDropChance() * 100.0) {
                 stack.stackSize = entry.getMinimumDropQuantity();
-                if(entry.getRandomDropQuantity() > 0) {
+                if (entry.getRandomDropQuantity() > 0) {
                     stack.stackSize = stack.stackSize + RANDOM.nextInt(entry.getRandomDropQuantity() + 1);
                 }
             }
@@ -123,8 +131,8 @@ public class TabLootHints extends TabWithTexture {
         this.slots[0] = new Integer[]{55, 4};
 
         //loots slot
-        for(int i = 0; i < 28; i++) {
-            this.slots[i + 1] = new Integer[] { 1 + (i % 7) * 18, 40 + (i / 7) * 18 };
+        for (int i = 0; i < 28; i++) {
+            this.slots[i + 1] = new Integer[]{1 + (i % 7) * 18, 40 + (i / 7) * 18};
         }
     }
 
@@ -137,7 +145,7 @@ public class TabLootHints extends TabWithTexture {
     public ItemStack[][] getItems(int index, ItemStack filter) {
         ItemStack[][] items = new ItemStack[this.recipesPerPage][];
 
-        for(int j = 0; j < this.recipesPerPage; ++j) {
+        for (int j = 0; j < this.recipesPerPage; ++j) {
             items[j] = new ItemStack[this.slots.length];
             int k = index + j;
 
@@ -186,7 +194,7 @@ public class TabLootHints extends TabWithTexture {
     }
 
     public boolean isAimedAtLoot(ItemStack itemStack) {
-        if(itemStack == null) {
+        if (itemStack == null) {
             return false;
         }
 
@@ -195,17 +203,18 @@ public class TabLootHints extends TabWithTexture {
                 return true;
             }
         }
+
         return false;
     }
 
     public float getChanceOf(ItemStack stack) {
-        if(stack == null) {
+        if (stack == null) {
             return 0.0f;
         }
 
-        for(LootTableEntry entry : currentLoot.lootTable.getLootTableEntries()) {
-            if(entry.getItemStack().getItemDamage() == stack.getItemDamage() && entry.getItemStack().getItemID() == stack.getItemID()) {
-                return Math.round(entry.getDropChance() * 100.0f);
+        for (LootTableEntry entry : currentLoot.lootTable.getLootTableEntries()) {
+            if (entry.getItemStack().getItemDamage() == stack.getItemDamage() && entry.getItemStack().getItemID() == stack.getItemID()) {
+                return Math.round(10000F * (entry.getItemWeight() / (float) currentLoot.totalWeight)) / 100F;
             }
         }
 
@@ -213,19 +222,19 @@ public class TabLootHints extends TabWithTexture {
     }
 
     public int[] getPossibleAmountOf(ItemStack stack) {
-        if(stack == null) {
+        if (stack == null) {
             return null;
         }
 
-        for(LootTableEntry entry : currentLoot.lootTable.getLootTableEntries()) {
-            if(entry.getItemStack().getItemDamage() == stack.getItemDamage() && entry.getItemStack().getItemID() == stack.getItemID()) {
-                if(entry.getRandomDropQuantity() == 0) {
-                    return new int[] {
+        for (LootTableEntry entry : currentLoot.lootTable.getLootTableEntries()) {
+            if (entry.getItemStack().getItemDamage() == stack.getItemDamage() && entry.getItemStack().getItemID() == stack.getItemID()) {
+                if (entry.getRandomDropQuantity() == 0) {
+                    return new int[]{
                             entry.getMinimumDropQuantity()
                     };
                 }
 
-                return new int[] {
+                return new int[]{
                         entry.getMinimumDropQuantity(),
                         entry.getMinimumDropQuantity() + entry.getRandomDropQuantity()
                 };
@@ -254,41 +263,41 @@ public class TabLootHints extends TabWithTexture {
         );
 
         //CLOVER
-        final float fixed1p13chance = (float) 1 /13;
+        final float fixed1p13chance = (float) 1 / 13;
         //clover doesn't have LootTable, so I created a copy based on chances from pickLootItem
         data[2] = new FancyPackedLoot(
                 new ItemStack(Blocks.CLOVER),
                 new LootTable(
-                        new LootTableEntry(new ItemStack(Items.STICKY_TORCH), fixed1p13chance, 1, 7),
-                        new LootTableEntry(new ItemStack(Items.GOLD_INGOT), fixed1p13chance, 1, 4),
-                        new LootTableEntry(new ItemStack(Items.DYNAMITE), fixed1p13chance, 1, 4),
-                        new LootTableEntry(new ItemStack(Items.EMPTY_BUCKET), fixed1p13chance),
-                        new LootTableEntry(new ItemStack(Items.POTION), fixed1p13chance),
-                        new LootTableEntry(new ItemStack(Items.CHAINMAIL_HELMET), fixed1p13chance * 1/4),
-                        new LootTableEntry(new ItemStack(Items.CHAINMAIL_CHESTPLATE), fixed1p13chance * 1/4),
-                        new LootTableEntry(new ItemStack(Items.CHAINMAIL_LEGGINGS), fixed1p13chance * 1/4),
-                        new LootTableEntry(new ItemStack(Items.CHAINMAIL_BOOTS), fixed1p13chance * 1/4),
-                        new LootTableEntry(new ItemStack(Items.THIRTEEN_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.CAT_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.BLOCKS_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.CHIRP_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.FAR_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.MALL_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.MELLOHI_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.STAL_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.STRAD_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.WARD_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.ELEVEN_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.WAIT_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.DOG_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.FAE_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.FROZEN_RECORD), fixed1p13chance * 1/15),
-                        new LootTableEntry(new ItemStack(Items.IRON_INGOT), fixed1p13chance, 1, 4),
-                        new LootTableEntry(new ItemStack(Items.WHEAT), fixed1p13chance, 1, 5),
-                        new LootTableEntry(new ItemStack(Items.GUNPOWDER), fixed1p13chance, 1, 5),
-                        new LootTableEntry(new ItemStack(Items.BREAD), fixed1p13chance, 1, 4),
-                        new LootTableEntry(new ItemStack(Blocks.SPONGE), fixed1p13chance, 1, 7),
-                        new LootTableEntry(new ItemStack(Items.SADDLE), fixed1p13chance)
+                        new LootTableEntry(1, new ItemStack(Items.STICKY_TORCH), fixed1p13chance, 1, 7),
+                        new LootTableEntry(1, new ItemStack(Items.GOLD_INGOT), fixed1p13chance, 1, 4),
+                        new LootTableEntry(1, new ItemStack(Items.DYNAMITE), fixed1p13chance, 1, 4),
+                        new LootTableEntry(1, new ItemStack(Items.EMPTY_BUCKET), fixed1p13chance),
+                        new LootTableEntry(1, new ItemStack(Items.POTION), fixed1p13chance),
+                        new LootTableEntry(1, new ItemStack(Items.CHAINMAIL_HELMET), fixed1p13chance * 1 / 4),
+                        new LootTableEntry(1, new ItemStack(Items.CHAINMAIL_CHESTPLATE), fixed1p13chance * 1 / 4),
+                        new LootTableEntry(1, new ItemStack(Items.CHAINMAIL_LEGGINGS), fixed1p13chance * 1 / 4),
+                        new LootTableEntry(1, new ItemStack(Items.CHAINMAIL_BOOTS), fixed1p13chance * 1 / 4),
+                        new LootTableEntry(1, new ItemStack(Items.THIRTEEN_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.CAT_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.BLOCKS_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.CHIRP_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.FAR_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.MALL_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.MELLOHI_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.STAL_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.STRAD_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.WARD_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.ELEVEN_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.WAIT_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.DOG_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.FAE_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.FROZEN_RECORD), fixed1p13chance * 1 / 15),
+                        new LootTableEntry(1, new ItemStack(Items.IRON_INGOT), fixed1p13chance, 1, 4),
+                        new LootTableEntry(1, new ItemStack(Items.WHEAT), fixed1p13chance, 1, 5),
+                        new LootTableEntry(1, new ItemStack(Items.GUNPOWDER), fixed1p13chance, 1, 5),
+                        new LootTableEntry(1, new ItemStack(Items.BREAD), fixed1p13chance, 1, 4),
+                        new LootTableEntry(1, new ItemStack(Blocks.SPONGE), fixed1p13chance, 1, 7),
+                        new LootTableEntry(1, new ItemStack(Items.SADDLE), fixed1p13chance)
                 )
         );
 
@@ -306,10 +315,14 @@ public class TabLootHints extends TabWithTexture {
         }
     }
 
+    /*
+     * basic helper code for showing tooltip details
+     * based on basically checking if the current GUI points to this tab,
+     * returns null otherwise
+     */
     public static TabLootHints tooltipHelper(GuiScreen screen) {
         if (screen instanceof GuiRecipeViewer viewer) {
-            Object o = GuiRecipeViewer.tabs.get(viewer.tabIndex);
-            if(o instanceof TabLootHints tab) {
+            if (GuiRecipeViewer.tabs.get(viewer.tabIndex) instanceof TabLootHints tab) {
                 return tab;
             }
         }
